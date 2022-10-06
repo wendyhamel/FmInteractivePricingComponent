c87b3928964ccf7a6783b6cb8cb3240bf343a428
wendyhamel <whamel@it4web.nl>
2022-10-06 16:04:39 +0200

    Added progress fill for all browser support

--- script/pricing.js
+++ script/pricing.js
@@ -19,6 +19,15 @@ window.pricing = function () {
 
         get amount() {
             return this.yearlyBilling ? (12*this.selectedItem.monthAmount)*0.75 : this.selectedItem.monthAmount;
+        },
+
+        progress() {
+            for (let e of document.querySelectorAll('.slider[type="range"].slider-progress')) {
+                e.style.setProperty('--value', e.value);
+                e.style.setProperty('--min', e.min == '' ? '0' : e.min);
+                e.style.setProperty('--max', e.max == '' ? '100' : e.max);
+                e.addEventListener('input', () => e.style.setProperty('--value', e.value));
+            }
         }
     }
 }
